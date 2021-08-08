793a6c1e9331eb627732c9c98650e5ae43319965
midbel <brun.nicolas21@gmail.com>
2021-08-08 12:43:41 +0200

    fix pie chart to deal properly with Arc attributes

--- chart/pie.go
+++ chart/pie.go
@@ -10,8 +10,9 @@ import (
 )
 
 const (
-	circum  = 360.0
-	deg2rad = math.Pi / 180
+	fullcirc  = 360.0
+	halfcirc  = 180.0
+	deg2rad = math.Pi / halfcirc
 )
 
 type PieChart struct {
@@ -33,33 +34,34 @@ func (c PieChart) render(w svg.Writer, serie Serie) {
 		dim    = svg.NewDim(c.Width, c.Height)
 		cs     = svg.NewSVG(dim.Option())
 		sum    = serie.Sum()
-		part   = circum / sum
+		part   = fullcirc / sum
 		cx, cy = c.GetAreaCenter()
 		area   = svg.NewGroup(svg.WithID("area"), svg.WithTranslate(cx, cy))
 		angle  float64
 	)
 	for i, v := range serie.values {
-		var next valuelabel
-		if i == serie.Len()-1 {
-			next = serie.values[0]
-		} else {
-			next = serie.values[i+1]
-		}
-		angle += v.Value * part
 		var (
 			fill = svg.NewFill(colors.Set26[i%len(colors.Set26)])
 			pos1 = getPosFromAngle(angle*deg2rad, float64(c.MaxRadius))
-			pos2 = getPosFromAngle((angle+(next.Value*part))*deg2rad, float64(c.MaxRadius))
-			pos3 = getPosFromAngle((angle+(next.Value*part))*deg2rad, float64(c.MaxRadius-c.MinRadius))
+			pos2 = getPosFromAngle((angle+(v.Value*part))*deg2rad, float64(c.MaxRadius))
+			pos3 = getPosFromAngle((angle+(v.Value*part))*deg2rad, float64(c.MaxRadius-c.MinRadius))
 			pos4 = getPosFromAngle(angle*deg2rad, float64(c.MaxRadius-c.MinRadius))
 			pat  = svg.NewPath(svg.WithID(v.Label), fill.Option(), whitstrok.Option())
+			swap bool
 		)
+		if tmp := v.Value*part; tmp > halfcirc {
+			swap = true
+		}
 		pat.AbsMoveTo(pos1)
-		pat.AbsArcTo(pos2, float64(c.MaxRadius), float64(c.MaxRadius), 0, false, true)
+		pat.AbsArcTo(pos2, float64(c.MaxRadius), float64(c.MaxRadius), 0, swap, true)
 		pat.AbsLineTo(pos3)
-		pat.AbsArcTo(pos4, float64(c.MaxRadius-c.MinRadius), float64(c.MaxRadius-c.MinRadius), 0, false, false)
+		if c.MinRadius == c.MaxRadius {
+			pat.AbsArcTo(pos4, float64(c.MaxRadius-c.MinRadius), float64(c.MaxRadius-c.MinRadius), 0, swap, false)
+		}
 		pat.AbsLineTo(pos1)
 		area.Append(pat.AsElement())
+
+		angle += v.Value * part
 	}
 	cs.Append(area.AsElement())
 	cs.Render(w)
--- examples/pie.go
+++ examples/pie.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+  "io"
 
 	"github.com/midbel/svg/chart"
 )
@@ -11,20 +12,25 @@ func main() {
 		c chart.PieChart
 		s chart.Serie
 	)
-	c.MaxRadius = 300
-	// c.MinRadius = 150
-	s.Add("toml", 100)
+  var w io.Writer = os.Stdout
+  // w = ioutil.Discard
+  c.Padding = chart.CreatePadding(20, 20)
+  c.Width = 400
+  c.Height = 400
+	c.MaxRadius = 150
+	c.MinRadius = 50
+	s.Add("toml", 150)
 	s.Add("json", 78)
 	s.Add("xml", 12)
-	// s.Add("ber", 70)
-	// s.Add("cbor", 80)
-	// s.Add("ldap", 98)
-	// s.Add("transmit", 200)
-	// s.Add("achile", 100)
-	// s.Add("prospect", 50)
-	// s.Add("svg", 178)
-	// s.Add("hadock", 500)
-	// s.Add("assist", 280)
-	// s.Add("inspect", 270)
-	c.Render(os.Stdout, s)
+	s.Add("ber", 70)
+	s.Add("cbor", 80)
+	s.Add("ldap", 98)
+	s.Add("transmit", 200)
+	s.Add("achile", 100)
+	s.Add("prospect", 50)
+	s.Add("svg", 178)
+	s.Add("hadock", 500)
+	s.Add("assist", 280)
+	s.Add("inspect", 270)
+	c.Render(w, s)
 }
