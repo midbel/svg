c1ba6a627b1f3c79a137e9f15ea8110fd130ce14
midbel <brun.nicolas21@gmail.com>
2021-08-09 19:30:56 +0200

    rename colours to colors

---
+++ colors.go
@@ -0,0 +1,301 @@
+package svg
+
+const (
+	Black                = 0x000000
+	Silver               = 0xC0C0C0
+	Gray                 = 0x808080
+	White                = 0xFFFFFF
+	Maroon               = 0x800000
+	Red                  = 0xFF0000
+	Purple               = 0x800080
+	Green                = 0x008000
+	Lime                 = 0x00FF00
+	Olive                = 0x808000
+	Yellow               = 0xFFFF00
+	Navy                 = 0x000080
+	Aqua                 = 0x00FFFF
+	AliceBlue            = 0xF0F8FF
+	AntiqueWhite         = 0xFAEBD7
+	Aquamarine           = 0x7FFFD4
+	Azure                = 0xF0FFFF
+	Beige                = 0xF5F5DC
+	Bisque               = 0xFFE4C4
+	BlanchedAlmond       = 0xFFEBCD
+	Blue                 = 0x0000FF
+	Blueviolet           = 0x8A2BE2
+	Brown                = 0xA52A2A
+	Burlywood            = 0xDEB887
+	CadetBlue            = 0x5F9EA0
+	Chartreuse           = 0x7FFF00
+	Chocolate            = 0xD2691E
+	Coral                = 0xFF7F50
+	CornFlowerBlue       = 0x6495ED
+	Cornsilk             = 0xFFF8DC
+	Crimson              = 0xDC143C
+	Cyan                 = 0x00FFFF
+	DarkBlue             = 0x00008B
+	DarkCyan             = 0x008B8B
+	DarkGoldenrod        = 0xB8860B
+	DarkGray             = 0xA9A9A9
+	DarkGreen            = 0x006400
+	DarkGrey             = 0xA9A9A9
+	DarkKhaki            = 0xBDB76B
+	DarkMagenta          = 0x8B008B
+	DarkOliveGreen       = 0x556B2F
+	DarkOrange           = 0xFF8C00
+	DarkOrchid           = 0x9932CC
+	DarkRed              = 0x8B0000
+	DarkSalmon           = 0xE9967A
+	DarkSeaGreen         = 0x8FBC8F
+	DarkSlateBlue        = 0x483D8B
+	DarkSlateGray        = 0x2F4F4F
+	DarkSlateGrey        = 0x2F4F4F
+	DarkTurquoise        = 0x00CED1
+	DarkViolet           = 0x9400D3
+	DeepPink             = 0xFF1493
+	DeepSkyBlue          = 0x00BFFF
+	DimGray              = 0x696969
+	DimGrey              = 0x696969
+	DodgerBlue           = 0x1E90FF
+	FireBrick            = 0xB22222
+	FloralWhite          = 0xFFFAF0
+	ForestGreen          = 0x228B22
+	Fuchsia              = 0xFF00FF
+	Gainsboro            = 0xDCDCDC
+	GhostWhite           = 0xF8F8FF
+	Gold                 = 0xFFD700
+	GoldenRod            = 0xDAA520
+	GreenYellow          = 0xADFF2F
+	Grey                 = 0x808080
+	Honeydew             = 0xF0FFF0
+	HotPink              = 0xFF69B4
+	IndianRed            = 0xCD5C5C
+	Indigo               = 0x4B0082
+	Ivory                = 0xFFFFF0
+	Khaki                = 0xF0E68C
+	Lavender             = 0xE6E6FA
+	LavenderBlush        = 0xFFF0F5
+	LawnGreen            = 0x7CFC00
+	LemonChiffon         = 0xFFFACD
+	LightBlue            = 0xADD8E6
+	LightCoral           = 0xF08080
+	LightCyan            = 0xE0FFFF
+	LightGoldenrodYellow = 0xFAFAD2
+	LightGray            = 0xD3D3D3
+	LightGreen           = 0x90EE90
+	LightGrey            = 0xD3D3D3
+	LightPink            = 0xFFB6C1
+	LightSalmon          = 0xFFA07A
+	LightSeaGreen        = 0x20B2AA
+	LightSkyBlue         = 0x87CEFA
+	LightSlateGray       = 0x778899
+	LightSlateGrey       = 0x778899
+	LightSteelBlue       = 0xB0C4DE
+	LightYellow          = 0xFFFFE0
+	Limegreen            = 0x32CD32
+	Linen                = 0xFAF0E6
+	Magenta              = 0xFF00FF
+	MediumAquamarine     = 0x66CDAA
+	MediumBlue           = 0x0000CD
+	MediumOrchid         = 0xBA55D3
+	MediumPurple         = 0x9370DB
+	MediumSeaGreen       = 0x3CB371
+	MediumSlateBlue      = 0x7B68EE
+	MediumSpringGreen    = 0x00FA9A
+	MediumTurquoise      = 0x48D1CC
+	MediumVioletRed      = 0xC71585
+	MidnightBlue         = 0x191970
+	MintCream            = 0xF5FFFA
+	MistyRose            = 0xFFE4E1
+	Moccasin             = 0xFFE4B5
+	NavajoWhite          = 0xFFDEAD
+	Oldlace              = 0xFDF5E6
+	Olivedrab            = 0x6B8E23
+	Orange               = 0xFFA500
+	OrangeRed            = 0xFF4500
+	Orchid               = 0xDA70D6
+	Palegoldenrod        = 0xEEE8AA
+	PaleGreen            = 0x98FB98
+	PaleTurquoise        = 0xAFEEEE
+	PaleVioletRed        = 0xDB7093
+	Papayawhip           = 0xFFEFD5
+	Peachpuff            = 0xFFDAB9
+	Peru                 = 0xCD853F
+	Pink                 = 0xFFC0CB
+	Plum                 = 0xDDA0DD
+	PowderBlue           = 0xB0E0E6
+	RosyBrown            = 0xBC8F8F
+	RoyalBlue            = 0x4169E1
+	SaddleBrown          = 0x8B4513
+	Salmon               = 0xFA8072
+	SandyBrown           = 0xF4A460
+	SeaGreen             = 0x2E8B57
+	Seashell             = 0xFFF5EE
+	Sienna               = 0xA0522D
+	SkyBlue              = 0x87CEEB
+	SlateBlue            = 0x6A5ACD
+	SlateGray            = 0x708090
+	SlateGrey            = 0x708090
+	Snow                 = 0xFFFAFA
+	SpringGreen          = 0x00FF7F
+	SteelBlue            = 0x4682B4
+	Tan                  = 0xD2B48C
+	Teal                 = 0x008080
+	Thistle              = 0xD8BFD8
+	Tomato               = 0xFF6347
+	Turquoise            = 0x40E0D0
+	Violet               = 0xEE82EE
+	Wheat                = 0xF5DEB3
+	WhiteSmoke           = 0xF5F5F5
+	YellowGreen          = 0x9ACD32
+)
+
+var Colours = []string{
+	"black",
+	"silver",
+	"gray",
+	"white",
+	"maroon",
+	"red",
+	"purple",
+	"green",
+	"lime",
+	"olive",
+	"yellow",
+	"navy",
+	"aqua",
+	"aliceblue",
+	"antiquewhite",
+	"aquamarine",
+	"azure",
+	"beige",
+	"bisque",
+	"blanchedalmond",
+	"blue",
+	"blueviolet",
+	"brown",
+	"burlywood",
+	"cadetblue",
+	"chartreuse",
+	"chocolate",
+	"coral",
+	"cornflowerblue",
+	"cornsilk",
+	"crimson",
+	"cyan",
+	"darkblue",
+	"darkcyan",
+	"darkgoldenrod",
+	"darkgray",
+	"darkgreen",
+	"darkgrey",
+	"darkkhaki",
+	"darkmagenta",
+	"darkolivegreen",
+	"darkorange",
+	"darkorchid",
+	"darkred",
+	"darksalmon",
+	"darkseagreen",
+	"darkslateblue",
+	"darkslategray",
+	"darkslategrey",
+	"darkturquoise",
+	"darkviolet",
+	"deeppink",
+	"deepskyblue",
+	"dimgray",
+	"dimgrey",
+	"dodgerblue",
+	"firebrick",
+	"floralwhite",
+	"forestgreen",
+	"fuchsia",
+	"gainsboro",
+	"ghostwhite",
+	"gold",
+	"goldenrod",
+	"greenyellow",
+	"grey",
+	"honeydew",
+	"hotpink",
+	"indianred",
+	"indigo",
+	"ivory",
+	"khaki",
+	"lavender",
+	"lavenderblush",
+	"lawngreen",
+	"lemonchiffon",
+	"lightblue",
+	"lightcoral",
+	"lightcyan",
+	"lightgoldenrodyellow",
+	"lightgray",
+	"lightgreen",
+	"lightgrey",
+	"lightpink",
+	"lightsalmon",
+	"lightseagreen",
+	"lightskyblue",
+	"lightslategray",
+	"lightslategrey",
+	"lightsteelblue",
+	"lightyellow",
+	"limegreen",
+	"linen",
+	"magenta",
+	"mediumaquamarine",
+	"mediumblue",
+	"mediumorchid",
+	"mediumpurple",
+	"mediumseagreen",
+	"mediumslateblue",
+	"mediumspringgreen",
+	"mediumturquoise",
+	"mediumvioletred",
+	"midnightblue",
+	"mintcream",
+	"mistyrose",
+	"moccasin",
+	"navajowhite",
+	"oldlace",
+	"olivedrab",
+	"orange",
+	"orangered",
+	"orchid",
+	"palegoldenrod",
+	"palegreen",
+	"paleturquoise",
+	"palevioletred",
+	"papayawhip",
+	"peachpuff",
+	"peru",
+	"pink",
+	"plum",
+	"powderblue",
+	"rosybrown",
+	"royalblue",
+	"saddlebrown",
+	"salmon",
+	"sandybrown",
+	"seagreen",
+	"seashell",
+	"sienna",
+	"skyblue",
+	"slateblue",
+	"slategray",
+	"slategrey",
+	"snow",
+	"springgreen",
+	"steelblue",
+	"tan",
+	"teal",
+	"thistle",
+	"tomato",
+	"turquoise",
+	"violet",
+	"wheat",
+	"whitesmoke",
+	"yellowgreen",
+}
