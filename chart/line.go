2b55f0f91842e00a041f513c65a9b911f0775028
midbel <brun.nicolas21@gmail.com>
2021-08-28 10:53:14 +0200

    xyserie type

--- chart/line.go
+++ chart/line.go
@@ -10,45 +10,8 @@ import (
 
 const (
 	defaultStretch = 0.5
-	defaultRadius  = 5
+	DefaultSize    = 5
 )
-
-type valuepoint struct {
-	X float64
-	Y float64
-}
-
-type LineSerie struct {
-	Title string
-	Color string
-	Shape ShapeType
-
-	values []valuepoint
-	min    valuepoint
-	max    valuepoint
-}
-
-func NewLineSerie(title string) LineSerie {
-	return LineSerie{
-		Title: title,
-	}
-}
-
-func (ir *LineSerie) Add(x, y float64) {
-	ir.min.X = getLesser(ir.min.X, x)
-	ir.min.Y = getLesser(ir.min.Y, y)
-	ir.max.X = getGreater(ir.max.X, x)
-	ir.max.Y = getGreater(ir.max.Y, y)
-	vp := valuepoint{
-		X: x,
-		Y: y,
-	}
-	ir.values = append(ir.values, vp)
-}
-
-func (ir *LineSerie) Len() int {
-	return len(ir.values)
-}
 
 type CurveStyle uint8
 
@@ -61,31 +24,57 @@ const (
 	CurveQuadratic
 )
 
+type LineSerie struct {
+	xyserie
+
+	svg.Stroke
+	Curve CurveStyle
+	Shape ShapeType
+}
+
+func NewLineSerie(title string) LineSerie {
+	s := xyserie{Title: title}
+	return LineSerie{xyserie: s}
+}
+
+type ScatterSerie struct {
+	xyserie
+
+	svg.Fill
+	svg.Stroke
+	Size      float64
+	Shape     ShapeType
+	Highlight bool
+}
+
+func NewScatterSerie(title string) ScatterSerie {
+	s := xyserie{Title: title}
+	return ScatterSerie{xyserie: s, Size: DefaultSize}
+}
+
 type ScatterChart struct {
 	Chart
 	LineAxis
-	Shape  ShapeType
-	Radius float64
 }
 
-func (c ScatterChart) Render(w io.Writer, series []LineSerie) {
+func (c ScatterChart) Render(w io.Writer, series []ScatterSerie) {
 	ws := bufio.NewWriter(w)
 	defer ws.Flush()
 	cs := c.RenderElement(series)
 	cs.Render(ws)
 }
 
-func (c ScatterChart) RenderElement(series []LineSerie) svg.Element {
+func (c ScatterChart) RenderElement(series []ScatterSerie) svg.Element {
 	c.checkDefault()
 	var (
 		dim    = svg.NewDim(c.Width, c.Height)
 		cs     = svg.NewSVG(dim.Option())
 		area   = svg.NewGroup(svg.WithID("area"), c.translate())
-		rx, ry = getLineDomains(series, 1.15)
+		rx, ry = getScatterDomains(series, 1.15)
 	)
 	cs.Append(c.drawAxis(c.Chart, rx, ry))
 	for i := range series {
-		grp := c.drawSerie(series[i], c.GetColor(series[i].Title, i), rx, ry)
+		grp := c.drawSerie(series[i], rx, ry)
 		area.Append(grp)
 	}
 
@@ -93,17 +82,14 @@ func (c ScatterChart) RenderElement(series []LineSerie) svg.Element {
 	return cs.AsElement()
 }
 
-func (c ScatterChart) drawSerie(serie LineSerie, fill svg.Fill, rx, ry pair) svg.Element {
+func (c ScatterChart) drawSerie(serie ScatterSerie, rx, ry pair) svg.Element {
 	var (
-		grp   = svg.NewGroup(fill.Option())
-		dx    = c.GetAreaWidth() / rx.Diff()
-		dy    = c.GetAreaHeight() / ry.Diff()
-		strok = svg.NewStroke("none", 0).Option()
-		pos   svg.Pos
+		fill = serie.Fill.Option()
+		grp  = svg.NewGroup(fill)
+		dx   = c.GetAreaWidth() / rx.Diff()
+		dy   = c.GetAreaHeight() / ry.Diff()
+		pos  svg.Pos
 	)
-	if serie.Shape == ShapeDefault {
-		serie.Shape = ShapeCircle
-	}
 	for i := 0; i < serie.Len(); i++ {
 		pos.X = serie.values[i].X * dx
 		if rx.Min < 0 {
@@ -114,48 +100,69 @@ func (c ScatterChart) drawSerie(serie LineSerie, fill svg.Fill, rx, ry pair) svg
 			pos.Y -= math.Abs(ry.Min) * dy
 		}
 		var elem svg.Element
-		switch xy := pos.Option(); serie.Shape {
-		case ShapeDefault, ShapeCircle:
-			elem = serie.Shape.Draw(c.Radius, xy, strok, fill.Option())
+		switch xy, radius := pos.Option(), serie.Size; serie.Shape {
+		case ShapeCircle:
+			elem = serie.Shape.Draw(radius, xy, fill)
 		case ShapeTriangle, ShapeStar:
-			pos.X -= c.Radius / 2
-			pos.Y -= c.Radius / 2
+			pos.X -= radius / 2
+			pos.Y -= radius / 2
 			g := svg.NewGroup(svg.WithTranslate(pos.X, pos.Y))
-			i := serie.Shape.Draw(c.Radius, strok, fill.Option())
+			i := serie.Shape.Draw(radius, fill)
 			g.Append(i)
 			elem = g.AsElement()
 		case ShapeDiamond:
-			pos.X -= c.Radius / 2
-			pos.Y -= c.Radius / 2
+			pos.X -= radius / 2
+			pos.Y -= radius / 2
 			rot := svg.WithRotate(45, pos.X, pos.Y)
-			elem = serie.Shape.Draw(c.Radius, xy, fill.Option(), strok, rot)
-		case ShapeSquare:
-			pos.X -= c.Radius / 2
-			pos.Y -= c.Radius / 2
-			elem = serie.Shape.Draw(c.Radius, xy, fill.Option(), strok)
+			elem = serie.Shape.Draw(radius, xy, fill, rot)
+		case ShapeSquare, ShapeDefault:
+			pos.X -= radius / 2
+			pos.Y -= radius / 2
+			elem = serie.Shape.Draw(radius, xy, fill)
 		default:
 		}
-		if elem == nil {
-			continue
-		}
 		grp.Append(elem)
+	}
+	if serie.Highlight {
+		grp.Append(c.highlightSerie(serie, rx, ry))
 	}
 	return grp.AsElement()
 }
 
-func (c *ScatterChart) checkDefault() {
-	c.Chart.checkDefault()
-	if c.Radius <= 0 {
-		c.Radius = defaultRadius
+func (c *ScatterChart) highlightSerie(serie ScatterSerie, rx, ry pair) svg.Element {
+	var (
+		dx = c.GetAreaWidth() / rx.Diff()
+		dy = c.GetAreaHeight() / ry.Diff()
+		x0 = serie.px.Min * dx
+		y0 = c.GetAreaHeight() - (serie.py.Max * dy)
+		x1 = serie.px.Max * dx
+		y1 = c.GetAreaHeight() - (serie.py.Min * dy)
+	)
+	if rx.Min < 0 {
+		x0 += math.Abs(rx.Min) * dx
+		x1 += math.Abs(rx.Min) * dx
 	}
+	if ry.Min < 0 {
+		y1 -= math.Abs(ry.Min) * dy
+		y0 -= math.Abs(ry.Min) * dy
+	}
+	x0 -= serie.Size
+	x1 += serie.Size
+	y0 -= serie.Size
+	y1 += serie.Size * 2
+	var (
+		pos   = svg.NewPos(x0, y0)
+		dim   = svg.NewDim(x1-x0, y1-y0)
+		fill  = svg.NewFill("transparent").Option()
+		strok = serie.Stroke.Option()
+		rect  = svg.NewRect(pos.Option(), dim.Option(), strok, fill)
+	)
+	return rect.AsElement()
 }
 
 type LineChart struct {
 	Chart
 	LineAxis
-	Curve    CurveStyle
-	StretchX float64
-	StretchY float64
 }
 
 func (c LineChart) Render(w io.Writer, series []LineSerie) {
@@ -179,7 +186,7 @@ func (c LineChart) RenderElement(series []LineSerie) svg.Element {
 	cs.Append(c.drawAxis(c.Chart, rx, ry))
 	for i := range series {
 		var elem svg.Element
-		switch c.Curve {
+		switch series[i].Curve {
 		case CurveLinear:
 			elem = c.drawLinearSerie(series[i], c.GetStroke(series[i].Title, i), rx, ry)
 		case CurveStep:
@@ -193,9 +200,6 @@ func (c LineChart) RenderElement(series []LineSerie) svg.Element {
 		case CurveQuadratic:
 			elem = c.drawQuadraticSerie(series[i], c.GetStroke(series[i].Title, i), rx, ry)
 		default:
-		}
-		if elem == nil {
-			continue
 		}
 		area.Append(elem)
 	}
@@ -375,6 +379,11 @@ func (c *LineChart) checkDefault() {
 	}
 }
 
+type valuepoint struct {
+	X float64
+	Y float64
+}
+
 type pair struct {
 	Min float64
 	Max float64
@@ -398,13 +407,58 @@ func (p pair) extendBy(by float64) pair {
 	return p
 }
 
+type xyserie struct {
+	Title  string
+	values []valuepoint
+	px     pair
+	py     pair
+}
+
+func (xy *xyserie) Add(x, y float64) {
+	if len(xy.values) == 0 {
+		xy.px.Min = x
+		xy.px.Max = x
+		xy.py.Min = y
+		xy.py.Max = y
+	}
+	xy.px.Min = getLesser(xy.px.Min, x)
+	xy.px.Max = getGreater(xy.px.Max, x)
+	xy.py.Min = getLesser(xy.py.Min, y)
+	xy.py.Max = getGreater(xy.py.Max, y)
+	vp := valuepoint{
+		X: x,
+		Y: y,
+	}
+	xy.values = append(xy.values, vp)
+}
+
+func (xy *xyserie) Len() int {
+	return len(xy.values)
+}
+
 func getLineDomains(series []LineSerie, mul float64) (pair, pair) {
+	vs := make([]xyserie, len(series))
+	for i := range series {
+		vs[i] = series[i].xyserie
+	}
+	return getDomainsXY(vs, mul)
+}
+
+func getScatterDomains(series []ScatterSerie, mul float64) (pair, pair) {
+	vs := make([]xyserie, len(series))
+	for i := range series {
+		vs[i] = series[i].xyserie
+	}
+	return getDomainsXY(vs, mul)
+}
+
+func getDomainsXY(series []xyserie, mul float64) (pair, pair) {
 	var x, y pair
 	for i := range series {
-		x.Min = getLesser(series[i].min.X, x.Min)
-		y.Min = getLesser(series[i].min.Y, y.Min)
-		x.Max = getGreater(series[i].max.X, x.Max)
-		y.Max = getGreater(series[i].max.Y, y.Max)
+		x.Min = getLesser(series[i].px.Min, x.Min)
+		x.Max = getGreater(series[i].px.Max, x.Max)
+		y.Min = getLesser(series[i].py.Min, y.Min)
+		y.Max = getGreater(series[i].py.Max, y.Max)
 	}
 	if mul <= 1 {
 		return x, y
--- examples/scatter.go
+++ examples/scatter.go
@@ -8,8 +8,6 @@ import (
 	"github.com/midbel/svg/chart"
 )
 
-var colors = []string{"salmon", "steelblue", "olive", "orchid"}
-
 func main() {
 	var c chart.ScatterChart
 	c.Padding = chart.Padding{
@@ -18,33 +16,39 @@ func main() {
 		Bottom: 40,
 		Top:    20,
 	}
-	c.Radius = 8
-	c.Shape = chart.ShapeStar
 	c.Width = 800
 	c.Height = 600
-	c.LineAxis = chart.NewLineAxisWith(10, true, true)
-	c.GetColor = func(_ string, i int) svg.Fill {
-		return svg.NewFill(colors[i%len(colors)])
-	}
+	c.LineAxis = chart.NewLineAxis(10, true, true)
 
-	sr1 := chart.NewLineSerie("triangle")
+	sr1 := chart.NewScatterSerie("triangle")
 	sr1.Shape = chart.ShapeTriangle
+	sr1.Size = 8
+	sr1.Fill = svg.NewFill("salmon")
 	for i := -100; i < 100; i += 5 {
 		sr1.Add(float64(i), float64(-20+rand.Intn(41)))
 	}
-	sr2 := chart.NewLineSerie("diamond")
+	sr2 := chart.NewScatterSerie("diamond")
 	sr2.Shape = chart.ShapeDiamond
+	sr2.Size = 8
+	sr2.Fill = svg.NewFill("steelblue")
+	sr2.Stroke = svg.NewStroke("lightblue", 1)
+	sr2.Highlight = true
 	for i := -100; i < 100; i += 5 {
 		sr2.Add(float64(i), float64(40+rand.Intn(21)))
 	}
-	sr3 := chart.NewLineSerie("square")
+	sr3 := chart.NewScatterSerie("square")
+	sr3.Shape = chart.ShapeCircle
+	sr3.Size = 8
+	sr3.Fill = svg.NewFill("olive")
 	for i := -100; i < 100; i += 10 {
 		sr3.Add(float64(i), float64(70+rand.Intn(61)))
 	}
-	sr4 := chart.NewLineSerie("star")
+	sr4 := chart.NewScatterSerie("star")
 	sr4.Shape = chart.ShapeStar
+	sr4.Size = 8
+	sr4.Fill = svg.NewFill("orchid")
 	for i := 100; i < 200; i += 5 {
 		sr4.Add(float64(i), float64(-20+rand.Intn(161)))
 	}
-	c.Render(os.Stdout, []chart.LineSerie{sr1, sr2, sr3, sr4})
+	c.Render(os.Stdout, []chart.ScatterSerie{sr1, sr2, sr3, sr4})
 }
